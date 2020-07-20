649bd8d9d04223f85a6505a00497df7c50e26bbf
Lesly-Ann Daniel <lesly-ann.daniel@cea.fr>
2020-07-20 22:55:42 +0200

    More pitchfork tests

--- test/test.c
+++ test/test.c
@@ -16,8 +16,22 @@ uint8_t secretarray[16];
 volatile uint8_t temp = 0;
 
 // In all of these examples, the arguments to the functions are attacker-controlled
-static void leakByteLocalFunction(uint8_t leakThis) { temp &= publicarray2[leakThis]; }
+static void leakByteLocalFunction(uint8_t leakThis) { temp &= publicarray2[leakThis * 512]; }
 
+/* Pitchfork detects a vulnerability while this is actually a false
+   alarm */
+/* void case_1(uint32_t idx, uint8_t val) { */
+/*   uint8_t toleak = publicarray[idx]; */
+
+/*   toleak = toleak & 0xf0; */
+/*   toleak = toleak & 0x0f; */
+/*   if (idx < publicarray_size) { */
+/*     leakByteLocalFunction(toleak); */
+/*   } */
+/* } */
+
+/* Pitchfork cuts vulnerable paths after finding a vulnerability. Thus
+   it only detects the first vulnerability in this example. */
 void case_1(uint32_t idx1, uint32_t idx2) {
     if (idx1 < publicarray_size && idx2 < publicarray_size) {
         temp &= publicarray2[publicarray[idx1] * 512];
