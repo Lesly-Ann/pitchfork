8f973f748f05a648b2303c7f46732c51c37ad527
Lesly-Ann Daniel <lesly-ann.daniel@cea.fr>
2020-07-20 18:46:21 +0200

    Modifier my test file

--- test/test.c
+++ test/test.c
@@ -1,39 +1,33 @@
 #include <stdint.h>
 
 // FORCEDINLINE definition from spectector-clang/13.c
-#ifdef __MSVC__
-#define FORCEDINLINE __forceinline
-#else
-#define FORCEDINLINE __attribute__((always_inline))
-#endif
+#define MASK(x, size) (x & (size - 1))
 
-uint64_t publicarray_size = 16;
-uint8_t publicarray[16] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
-uint8_t publicarray2[512 * 256] = { 20 };
+uint32_t publicarray_size = 16;
+uint8_t publicarray[16];
+uint8_t publicarray2[512 * 256];
 
 // The attacker's goal in all of these examples is to learn any of the secret data in secretarray
-uint64_t secretarray_size = 16;
-uint8_t secretarray[16] = { 10,21,32,43,54,65,76,87,98,109,110,121,132,143,154,165 };
+uint32_t secretarray_size = 16;
+uint8_t secretarray[16];
 
-// 'volatile' ensures it's actually in memory
-volatile uint8_t benignIndex = 0;
 
 // this is mostly used to prevent the compiler from optimizing out certain operations
 volatile uint8_t temp = 0;
 
 // In all of these examples, the arguments to the functions are attacker-controlled
+static void leakByteLocalFunction(uint8_t leakThis) { temp &= publicarray2[leakThis]; }
 
-void case_1(uint64_t idx, uint8_t val, uint64_t idx2) {
-
-    if (idx < publicarray_size) {
-      publicarray2[publicarray[idx] * 512] = val;
+void case_1(uint32_t idx1, uint32_t idx2) {
+    if (idx1 < publicarray_size && idx2 < publicarray_size) {
+        temp &= publicarray2[publicarray[idx1] * 512];
+        temp &= publicarray2[publicarray[idx2] * 512];
     }
-    
 }
 
 // Provided just so this can compile into a complete binary.
 // Clearly, these inputs will not result in leaked secrets themselves.
 int main() {
-    case_1(0, 0, 0);
+  case_1(0, 0);
     return 0;
 }
